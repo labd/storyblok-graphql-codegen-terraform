6d9e934056c23f839dd245cd16e4fc602f368960
Kors van Loon <k.vanloon@labdigital.nl>
2023-06-06 22:53:53 +0200

    format expected results

--- test/testdata/expected/component.tf
+++ test/testdata/expected/component.tf
@@ -1,95 +1,95 @@
-resource "storyblok_component" "default_nest"{
-name = "default_nest"
-space_id = 123
-is_root = false
-is_nestable = true
-schema = {
-name = {
-position = 0
-display_name = "Name"
-required = true
-type = "text"
-}
-}
-}
-
-resource "storyblok_component" "test"{
-name = "test"
-space_id = 123
-is_root = false
-is_nestable = true
-schema = {
-name = {
-position = 0
-display_name = "Name"
-required = false
-type = "text"
-}
-}
+resource "storyblok_component" "default_nest" {
+  name        = "default_nest"
+  space_id    = 123
+  is_root     = false
+  is_nestable = true
+  schema = {
+    name = {
+      position     = 0
+      display_name = "Name"
+      required     = true
+      type         = "text"
+    }
+  }
 }
 
-resource "storyblok_component" "content_type"{
-name = "content_type"
-space_id = 123
-is_root = true
-is_nestable = false
-schema = {
-name = {
-position = 0
-display_name = "Name"
-required = false
-type = "text"
-}
-}
-}
-
-resource "storyblok_component" "nestable"{
-name = "nestable"
-space_id = 123
-is_root = false
-is_nestable = true
-schema = {
-name = {
-position = 0
-display_name = "Name"
-required = false
-type = "text"
-}
-}
+resource "storyblok_component" "test" {
+  name        = "test"
+  space_id    = 123
+  is_root     = false
+  is_nestable = true
+  schema = {
+    name = {
+      position     = 0
+      display_name = "Name"
+      required     = false
+      type         = "text"
+    }
+  }
 }
 
-resource "storyblok_component" "universal"{
-name = "universal"
-space_id = 123
-is_root = true
-is_nestable = true
-schema = {
-name = {
-position = 0
-display_name = "Name"
-required = false
-type = "text"
-}
-}
-}
-
-resource "storyblok_component_group" "test_group"{
-name = "test-group"
-space_id = 123
+resource "storyblok_component" "content_type" {
+  name        = "content_type"
+  space_id    = 123
+  is_root     = true
+  is_nestable = false
+  schema = {
+    name = {
+      position     = 0
+      display_name = "Name"
+      required     = false
+      type         = "text"
+    }
+  }
 }
 
-resource "storyblok_component" "group"{
-name = "group"
-space_id = 123
-is_root = false
-is_nestable = true
-component_group_uuid = storyblok_component_group.test_group.uuid
-schema = {
-name = {
-position = 0
-display_name = "Name"
-required = false
-type = "text"
+resource "storyblok_component" "nestable" {
+  name        = "nestable"
+  space_id    = 123
+  is_root     = false
+  is_nestable = true
+  schema = {
+    name = {
+      position     = 0
+      display_name = "Name"
+      required     = false
+      type         = "text"
+    }
+  }
 }
+
+resource "storyblok_component" "universal" {
+  name        = "universal"
+  space_id    = 123
+  is_root     = true
+  is_nestable = true
+  schema = {
+    name = {
+      position     = 0
+      display_name = "Name"
+      required     = false
+      type         = "text"
+    }
+  }
 }
+
+resource "storyblok_component_group" "test_group" {
+  name     = "test-group"
+  space_id = 123
+}
+
+resource "storyblok_component" "group" {
+  name                 = "group"
+  space_id             = 123
+  is_root              = false
+  is_nestable          = true
+  component_group_uuid = storyblok_component_group.test_group.uuid
+  schema = {
+    name = {
+      position     = 0
+      display_name = "Name"
+      required     = false
+      type         = "text"
+    }
+  }
 }
--- test/testdata/expected/fields.tf
+++ test/testdata/expected/fields.tf
@@ -1,217 +1,217 @@
-resource "storyblok_component" "base"{
-name = "base"
-space_id = 123
-is_root = false
-is_nestable = true
-schema = {
-text = {
-position = 0
-display_name = "Text"
-required = true
-type = "text"
-max_length = 4
-}
-textarea = {
-position = 1
-display_name = "Textarea"
-required = true
-type = "textarea"
-}
-markdown = {
-position = 2
-display_name = "Markdown"
-required = true
-type = "markdown"
-}
-richtext = {
-position = 3
-display_name = "Richtext"
-required = true
-type = "markdown"
-rich_markdown = true
-}
-optionalRegexText = {
-position = 4
-display_name = "Optional regex text"
-required = false
-type = "text"
-regex = "/[\\w\\-]+"
-}
-integer = {
-position = 5
-display_name = "Integer"
-required = true
-type = "number"
-min_value = 2
-max_value = 10
-}
-optionalInteger = {
-position = 6
-display_name = "Optional integer"
-required = false
-type = "number"
-}
-float = {
-position = 7
-display_name = "Float"
-required = true
-type = "number"
-min_value = 0
-max_value = 10
-}
-optionalFloat = {
-position = 8
-display_name = "Optional float"
-required = false
-type = "number"
-}
-boolean = {
-position = 9
-display_name = "Boolean"
-required = true
-type = "boolean"
-}
-optionalBoolean = {
-position = 10
-display_name = "Optional boolean"
-required = false
-type = "boolean"
-}
-date = {
-position = 11
-display_name = "Date"
-required = false
-type = "datetime"
-disable_time = true
-}
-dateTime = {
-position = 12
-display_name = "Date time"
-required = false
-type = "datetime"
-}
-image = {
-position = 13
-display_name = "Image"
-required = true
-type = "asset"
-}
-assets = {
-position = 14
-display_name = "Assets"
-required = false
-type = "bloks"
-component_whitelist = [
-"storyblok_asset"
-]
-}
-option = {
-position = 15
-display_name = "Option"
-required = false
-type = "option"
-options = [
-[
-{
-name = "a"
-value = "a"
-},
-{
-name = "b"
-value = "b"
-}
-]
-]
-}
-options = {
-position = 16
-display_name = "Options"
-required = false
-type = "options"
-options = [
-[
-{
-name = "a"
-value = "a"
-},
-{
-name = "b"
-value = "b"
-}
-]
-]
-}
-blok = {
-position = 17
-display_name = "Blok"
-required = false
-type = "bloks"
-component_whitelist = [
-"a"
-]
-minimum = 0
-maximum = 1
-}
-bloks = {
-position = 18
-display_name = "Bloks"
-required = false
-type = "bloks"
-component_whitelist = [
-"b"
-]
-}
-unionBlok = {
-position = 19
-display_name = "Union blok"
-required = false
-type = "bloks"
-component_whitelist = [
-"a",
-"b"
-]
-minimum = 0
-maximum = 1
-}
-unionBloks = {
-position = 20
-display_name = "Union bloks"
-required = false
-type = "bloks"
-component_whitelist = [
-"a",
-"b"
-]
-}
-}
-}
-
-resource "storyblok_component" "a"{
-name = "a"
-space_id = 123
-is_root = false
-is_nestable = true
-schema = {
-a = {
-position = 0
-display_name = "A"
-required = true
-type = "text"
-}
-}
+resource "storyblok_component" "base" {
+  name        = "base"
+  space_id    = 123
+  is_root     = false
+  is_nestable = true
+  schema = {
+    text = {
+      position     = 0
+      display_name = "Text"
+      required     = true
+      type         = "text"
+      max_length   = 4
+    }
+    textarea = {
+      position     = 1
+      display_name = "Textarea"
+      required     = true
+      type         = "textarea"
+    }
+    markdown = {
+      position     = 2
+      display_name = "Markdown"
+      required     = true
+      type         = "markdown"
+    }
+    richtext = {
+      position      = 3
+      display_name  = "Richtext"
+      required      = true
+      type          = "markdown"
+      rich_markdown = true
+    }
+    optionalRegexText = {
+      position     = 4
+      display_name = "Optional regex text"
+      required     = false
+      type         = "text"
+      regex        = "/[\\w\\-]+"
+    }
+    integer = {
+      position     = 5
+      display_name = "Integer"
+      required     = true
+      type         = "number"
+      min_value    = 2
+      max_value    = 10
+    }
+    optionalInteger = {
+      position     = 6
+      display_name = "Optional integer"
+      required     = false
+      type         = "number"
+    }
+    float = {
+      position     = 7
+      display_name = "Float"
+      required     = true
+      type         = "number"
+      min_value    = 0
+      max_value    = 10
+    }
+    optionalFloat = {
+      position     = 8
+      display_name = "Optional float"
+      required     = false
+      type         = "number"
+    }
+    boolean = {
+      position     = 9
+      display_name = "Boolean"
+      required     = true
+      type         = "boolean"
+    }
+    optionalBoolean = {
+      position     = 10
+      display_name = "Optional boolean"
+      required     = false
+      type         = "boolean"
+    }
+    date = {
+      position     = 11
+      display_name = "Date"
+      required     = false
+      type         = "datetime"
+      disable_time = true
+    }
+    dateTime = {
+      position     = 12
+      display_name = "Date time"
+      required     = false
+      type         = "datetime"
+    }
+    image = {
+      position     = 13
+      display_name = "Image"
+      required     = true
+      type         = "asset"
+    }
+    assets = {
+      position     = 14
+      display_name = "Assets"
+      required     = false
+      type         = "bloks"
+      component_whitelist = [
+        "storyblok_asset"
+      ]
+    }
+    option = {
+      position     = 15
+      display_name = "Option"
+      required     = false
+      type         = "option"
+      options = [
+        [
+          {
+            name  = "a"
+            value = "a"
+          },
+          {
+            name  = "b"
+            value = "b"
+          }
+        ]
+      ]
+    }
+    options = {
+      position     = 16
+      display_name = "Options"
+      required     = false
+      type         = "options"
+      options = [
+        [
+          {
+            name  = "a"
+            value = "a"
+          },
+          {
+            name  = "b"
+            value = "b"
+          }
+        ]
+      ]
+    }
+    blok = {
+      position     = 17
+      display_name = "Blok"
+      required     = false
+      type         = "bloks"
+      component_whitelist = [
+        "a"
+      ]
+      minimum = 0
+      maximum = 1
+    }
+    bloks = {
+      position     = 18
+      display_name = "Bloks"
+      required     = false
+      type         = "bloks"
+      component_whitelist = [
+        "b"
+      ]
+    }
+    unionBlok = {
+      position     = 19
+      display_name = "Union blok"
+      required     = false
+      type         = "bloks"
+      component_whitelist = [
+        "a",
+        "b"
+      ]
+      minimum = 0
+      maximum = 1
+    }
+    unionBloks = {
+      position     = 20
+      display_name = "Union bloks"
+      required     = false
+      type         = "bloks"
+      component_whitelist = [
+        "a",
+        "b"
+      ]
+    }
+  }
 }
 
-resource "storyblok_component" "b"{
-name = "b"
-space_id = 123
-is_root = false
-is_nestable = true
-schema = {
-b = {
-position = 0
-display_name = "B"
-required = true
-type = "text"
+resource "storyblok_component" "a" {
+  name        = "a"
+  space_id    = 123
+  is_root     = false
+  is_nestable = true
+  schema = {
+    a = {
+      position     = 0
+      display_name = "A"
+      required     = true
+      type         = "text"
+    }
+  }
 }
-}
+
+resource "storyblok_component" "b" {
+  name        = "b"
+  space_id    = 123
+  is_root     = false
+  is_nestable = true
+  schema = {
+    b = {
+      position     = 0
+      display_name = "B"
+      required     = true
+      type         = "text"
+    }
+  }
 }
